d451ba2d41973227787e82b07db450ce49e6a55d
Soundharya <ssoundharyasoundharya07@gmail.com>
2025-08-25 22:12:57 +0530

    Add map touch feature and Firebase integration

---
+++ firebaseconfig.js
@@ -0,0 +1,20 @@
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgz7wpg1nkXZ9uuHyCgLRwAfZ_FzFAlJA",
+  authDomain: "travelmate-07chss.firebaseapp.com",
+  databaseURL: "https://travelmate-07chss-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "travelmate-07chss",
+  storageBucket: "travelmate-07chss.firebasestorage.app",
+  messagingSenderId: "985002644614",
+  appId: "1:985002644614:web:bba90799c119c92ef76d07"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export the Realtime Database instance
+export const db = getDatabase(app);
